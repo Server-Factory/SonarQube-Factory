125bc4eebea997e46246be291f0d130896d74a2a
Milos Vasic <milos85vasic@gmail.com>
2021-05-18 18:33:35 +0200

    MSF-435 - Caching Proxy, Alpha 2.

--- Application/src/main/kotlin/net/milosvasic/factory/proxy/caching/application/BuildInfo.kt
+++ Application/src/main/kotlin/net/milosvasic/factory/proxy/caching/application/BuildInfo.kt
@@ -4,9 +4,10 @@ import net.milosvasic.factory.application.BuildInformation
 
 object BuildInfo : BuildInformation {
 
-    override val version = "1.0.0 Alpha 1"
-    override val versionCode = (100 * 1000) + 0
-    override val versionName = "Caching Proxy Factory Client"
+    override val version = "1.0.0 Alpha 2"
+    override val versionCode = 2
+    override val versionName = "Caching Proxy Factory"
+    override val productName = "Caching-Proxy-Factory"
 
     override fun printName() = "$versionName $version"
 }
