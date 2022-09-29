2687faf0319896d49e9a9dc9de27c01a32e854c7
Milos Vasic <i@mvasic.ru>
2022-09-29 15:47:23 +0300

    Upstreamable.

---
+++ Upstreams/GitFlic.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export UPSTREAMABLE_REPOSITORY="git@gitflic.ru:server-factory/caching-proxy-factory.git"
---
+++ Upstreams/GitHub.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export UPSTREAMABLE_REPOSITORY="git@github.com:Server-Factory/Caching-Proxy-Factory.git"
